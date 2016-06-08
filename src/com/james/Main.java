8c2d66f32b51f72e777f78a45e80fa0628a14fb8
jamesyburr <vint.james@gmail.com>
2016-06-08 11:40:14 -0400

    add stuff

--- src/com/james/Main.java
+++ src/com/james/Main.java
@@ -1,5 +1,11 @@
 package com.james;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,6 +17,64 @@ public class Main {
     public static void main(String[] args) {
         addTestUsers();
         addTestMessage();
+
+        Spark.init();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+                    String username = session.attribute("username");
+
+                    String idStr = request.queryParams("replyId");
+                    int replyId = -1;
+                    if (idStr != null) {
+                        replyId = Integer.valueOf(idStr);
+                    }
+
+                    ArrayList<Message> subset = new ArrayList<Message>();
+                    for (Message msg : messages) {
+                        if (msg.replyId == replyId) {
+                            subset.add(msg);
+                        }
+                    }
+
+                    HashMap m = new HashMap();
+                    m.put("messages", subset);
+                    m.put("username", username);
+                    return  new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/login",
+                (request, response) -> {
+                    String username = request.queryParams("username");
+                    if (username == null) {
+                        throw new Exception("Login name not found");
+                    }
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username, "");
+                        users.put(username, user);
+                    }
+
+                    Session session = request.session();
+                    session.attribute("username", username);
+
+                    response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                }
+        );
 
     }
 
